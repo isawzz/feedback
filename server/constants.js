0fb4fbc3c78cc1ee2596c9f83486fa5625f69ff6
isawzz <iwgajam@gmail.com>
2022-08-21 11:33:01 -0700

    das

--- server/constants.js
+++ server/constants.js
@@ -1,8 +1,8 @@
 const FRAME_RATE = 5;
-const WINIT = 30;
+const WINIT = 50;
 const INCGREEN = 10; //inc w at plus 
 const INCRED = 5; 
-const VINIT = -0.005;
+const VINIT = -0.0005;
 const VINCGREEN = 0; 
 const VINCRED = 0;
 
